8a5ca368717f76b0b872def4bd6ed46c2dc71d6d
davidcoats <david@getsafetycone.com>
2023-07-08 14:20:02 -0700

    Initial commit.

---
+++ source/R5T.T0217/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0217
+{
+	/// <summary>
+	/// MSBuild output related path strong-types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0217/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0217
+{
+    public static class Instances
+    {
+		
+    }
+}
