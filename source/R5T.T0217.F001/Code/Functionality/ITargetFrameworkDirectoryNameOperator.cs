71514b577e1ee18cf839450976fb6911179ae7aa
davidcoats <david@getsafetycone.com>
2023-11-05 11:53:05 -0800

    Interim changes.

--- source/R5T.T0217.F001/Code/Functionality/ITargetFrameworkDirectoryNameOperator.cs
+++ source/R5T.T0217.F001/Code/Functionality/ITargetFrameworkDirectoryNameOperator.cs
@@ -13,11 +13,26 @@ namespace R5T.T0217.F001
     {
         public ITargetFrameworkDirectoryName Get_TargetFrameworkDirectoryName(ITargetFrameworkMoniker targetFrameworkMoniker)
         {
-            // The directory name is just the moniker name.
-            var output = targetFrameworkMoniker.Value
-                .ToTargetFrameworkDirectoryName();
+            // If windows, the directory name is just the non windows part.
+            var isWindows = Instances.TargetFrameworkMonikerOperator.Is_WindowsSpecific(targetFrameworkMoniker);
+            if (isWindows)
+            {
+                var nonWindowsMoniker = Instances.TargetFrameworkMonikerOperator.Get_NonWindowsMoniker(targetFrameworkMoniker);
 
-            return output;
+                // The directory name is just the moniker name.
+                var output = nonWindowsMoniker.Value
+                    .ToTargetFrameworkDirectoryName();
+
+                return output;
+            }
+            else
+            {
+                // The directory name is just the moniker name.
+                var output = targetFrameworkMoniker.Value
+                    .ToTargetFrameworkDirectoryName();
+
+                return output;
+            }
         }
     }
 }
--- source/R5T.T0217.F001/Code/Instances.cs
+++ source/R5T.T0217.F001/Code/Instances.cs
@@ -5,6 +5,6 @@ namespace R5T.T0217.F001
 {
     public static class Instances
     {
-		
+        public static L0056.ITargetFrameworkMonikerOperator TargetFrameworkMonikerOperator => L0056.TargetFrameworkMonikerOperator.Instance;
     }
 }
